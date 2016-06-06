64be700145ae8f41981dcf68dd2840b9f5c92b09
jomipeca <jose.perez@peluka.info>
2016-06-06 12:46:24 -0400

    subida 06-06 v2

--- src/cl/curso/java/guia12/ejercicio1/CarroCompras.java
+++ src/cl/curso/java/guia12/ejercicio1/CarroCompras.java
@@ -1,64 +1,89 @@
 package cl.curso.java.guia12.ejercicio1;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class CarroCompras {
-   private Usuario usuario;
-   private List<Producto> productos;
-   
-   public CarroCompras(){}
+	private Usuario usuario;
+	private List<Producto> productos;
 
-/**
- * @param usuario
- * @param productos
- */
-public CarroCompras(Usuario usuario, List<Producto> productos) {
-	super();
-	this.usuario = usuario;
-	this.productos = productos;
-}
+	
 
-public Usuario getUsuario() {
-	return usuario;
-}
-
-public void setUsuario(Usuario usuario) {
-	this.usuario = usuario;
-}
-
-public List<Producto> getProductos() {
-	return productos;
-}
-
-public void setProductos(List<Producto> productos) {
-	this.productos = productos;
-}
-   
-   public void agregarProducto(Producto producto){
-	   this.getProductos().add(producto);
-   }
-   
-   public void eliminarProducto(Producto producto){
-	   if(this.getProductos().contains(producto)){
-	   this.getProductos().remove(producto);
-	   }
-	   else{
-		   throw new ProductoNoEncontradoException("no existe el producto");
-	   }
-   }
-   
-   public void ordenarPorPrecio(){
-	   
-	   
-	   Collections.sort(this.getProductos());
-   }
-   
-   public void imprimir(){
-	   for (int i = 0; i < this.getProductos().size(); i++) {
-		System.out.println(this.getProductos().get(i));
+	/**
+	 * Constructor que inicializa parametros
+	 */
+	public CarroCompras() {
+		this.usuario = null;
+		this.productos = new ArrayList<Producto>();
 	}
-   }
-   
+
+	/**
+	 * Constructor que recibe parametros
+	 * @param usuario
+	 * @param productos
+	 */
+	public CarroCompras(Usuario usuario, List<Producto> productos) {
+		this.usuario = usuario;
+		this.productos = productos;
+	}
+
+	
+
+	/**
+	 * Retorna el parametro usuario (Accesador)
+	 * @return usuario
+	 */
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * Mutador usuario
+	 * @param Modifica el usuario y lo reemplaza con usuario
+	 */
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * Retorna el parametro productos (Accesador)
+	 * @return productos
+	 */
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	/**
+	 * Mutador productos
+	 * @param Modifica el productos y lo reemplaza con productos
+	 */
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
+
+	public void agregarProducto(Producto producto) {
+		this.getProductos().add(producto);
+	}
+
+	public void eliminarProducto(Producto producto) {
+		if (this.getProductos().contains(producto)) {
+			this.getProductos().remove(producto);
+		} else {
+			throw new ProductoNoEncontradoException("no existe el producto");
+		}
+	}
+
+	public void ordenarPorPrecio() {
+
+		Collections.sort(this.getProductos());
+	}
+
+	public void imprimir() {
+		for (int i = 0; i < this.getProductos().size(); i++) {
+			System.out.println(this.getProductos().get(i));
+		}
+	}
+
 }
